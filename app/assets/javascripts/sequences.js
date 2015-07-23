33f558b1d8f142de5b3251433fdadc3658de40ba
Matej Minarik <matej.minarik@agile-ict.com>
2015-07-23 17:40:35 +0200

    Movement between frames

--- app/assets/javascripts/sequences.js
+++ app/assets/javascripts/sequences.js
@@ -1,27 +1,91 @@
 $(document).ready(function() {
-var color = "#FF0000";
-var sequence = [];
+	var color = 'red';
+	var sequence = [];
+
+	function saveState() {
+		var local_sequence = {};
+		$(".hostel-win").each(function(){
+			var color = $(this).css('background-color');
+			var id = $(this).attr('id');
+			local_sequence[id] = color;
+		});
+		sequence.push(local_sequence);
+		local_sequence = {};
+	}
+
+	function getPreviousState() {
+		var position = getCurrentPosition();
+		var local_sequence = sequence[position - 2];
+
+		$(".hostel-win").each(function(){
+			var id = $(this).attr('id');
+			$(this).css({'background-color':local_sequence[id]});
+		});
+
+		$("#position").text(position - 1);
+	}
+
+	function getNextState() {
+		var position = getCurrentPosition();
+		var local_sequence = sequence[position];
+
+		$(".hostel-win").each(function(){
+			var id = $(this).attr('id');
+			$(this).css({'background-color':local_sequence[id]});
+		});
+
+		$("#position").text(position + 1);
+	}
+
+	function getCurrentPosition() {
+		return +$("#position").text();
+	}
+
+	function getNoPositions() {
+		return +$("#positions").text();
+	}
 
 	$(function() {    
-		$( ".hostel-win" ).click(function(event) {
-		  $( "#" + event.target.id ).animate({
+		$(".hostel-win").click(function(event) {
+		  $( "#" + event.target.id ).css({'background-color': color});
+		  /*$( "#" + event.target.id ).animate({
 		    backgroundColor: color,
-		  }, 1000 );
+		  }, 'fast' );*/
 		});
 	});
 
 	$(function() {    
-		$( "#green" ).click(function(event) {
-		 	color = "#00FF00"; 
+		$("#green").click(function(event) {
+		 	color = 'green'; 
 		 });
 	});
 
 	$(function() {    
-		$( "#red" ).click(function(event) {
-		 	color = "#FF0000"; 
+		$("#red").click(function(event) {
+		 	color = 'red'; 
 		 });
 	});
 
-	
+	$("#next").on('click', function(){
+		var position = getCurrentPosition();
+		var positions = getNoPositions();
+
+		if(position < positions){
+			getNextState();
+		} else {
+			saveState();
+			if(position == positions){
+				$("#position").text(position + 1);
+				$("#positions").text(positions + 1);
+			}
+		}
+	});
+
+	$("#prev").on('click', function(){
+		
+		saveState();
+		getPreviousState();
+
+	});
 
 });
