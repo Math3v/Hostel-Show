9fdd60939d0844563da826284011118df9ef8e33
Matej Minarik <matej.minarik@agile-ict.com>
2015-08-03 22:16:20 +0200

    Flash messages with bootstrap design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def twitterized_type(type)
+	  case type.to_s
+	    when "alert"
+	      "block"
+	    when "error"
+	      "error"
+	    when "notice"
+	      "info"
+	    when "success"
+	      "success"
+	    else
+	      type.to_s
+	  end
+	end
 end
