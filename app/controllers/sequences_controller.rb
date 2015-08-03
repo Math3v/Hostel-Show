e056a8f568ad9434da6c90314b04f52e89435d96
Matej Minarik <matej.minarik@agile-ict.com>
2015-08-03 22:54:58 +0200

    Added user_id as permit param

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -77,6 +77,6 @@ class SequencesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sequence_params
-      params.require(:sequence).permit(:title, :description, :data)
+      params.require(:sequence).permit(:title, :description, :data, :user_id)
     end
 end
