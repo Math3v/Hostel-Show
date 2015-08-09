bcd3597a304fee9d977ce9a85de3733be97f344d
Matej Minarik <matej.minarik@agile-ict.com>
2015-08-09 21:15:05 +0200

    Added sequence model test

--- test/models/sequence_test.rb
+++ test/models/sequence_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class SequenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "average rating should return valid rating" do
+  	sequence = Sequence.create(title: 'Title', description: 'Description')
+		rating_1 = Rating.create(sequence_id: sequence.id, score: 3)
+		rating_2 = Rating.create(sequence_id: sequence.id, score: 4)
+    
+    avg_rating = sequence.average_rating
+    assert avg_rating == 3.5
+  end
+
+  test "validates title presence" do
+  	sequence = Sequence.create(title: "")
+  	assert_not sequence.save
+  end
 end
