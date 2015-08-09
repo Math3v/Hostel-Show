a8f660db0a68ec41d47e3e1ec8e32622194fac30
Matej Minarik <matej.minarik@agile-ict.com>
2015-08-09 22:10:29 +0200

    Added Location model tests

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "validates name presence" do
+    location = Location.create(floors: 5, per_floor: 3)
+    assert_not location.save
+  end
+
+  test "validates floors presence" do
+    location = Location.create(name: 'Location', per_floor: 3)
+    assert_not location.save
+  end
+
+  test "validates per_floor presence" do
+    location = Location.create(name: 'Location', floors: 5)
+    assert_not location.save
+  end
+
+  test "validates valid location save" do
+  	location = Location.create(name: 'Location', floors: 3, per_floor: 2)
+  	assert location.save
+  end
 end
